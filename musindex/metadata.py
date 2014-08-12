0cc0d39d4cec694622942bcda626839e7980e456
huafeng_huang <huanghuafeng@cmos.net>
2014-08-12 20:20:43 +0800

    add mutagen doc for reference

--- musindex/metadata.py
+++ musindex/metadata.py
@@ -1,2 +1,16 @@
+import mutagen
 
 metadata = mutagen.File('/home/lenny/音乐/The_Band_Perry_If_I_Die_Young.flac')
+
+class MusicFile(object):
+    def __init__(self, name):
+        pass
+
+    def get_filetype(self):
+        pass
+
+    def get_metadata(self):
+        pass
+
+    def set_metadata(self, **args):
+        pass
